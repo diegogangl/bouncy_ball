5b7d63245beb0f1a94ced587e77c438c4083f6e8
Diego Gangl <diego@sinestesia.co>
2017-04-06 21:47:05 -0300

    Add line drawing to draw_circle()
    ...and start using it

--- ball.py
+++ ball.py
@@ -34,9 +34,11 @@ def draw(position):
 
     radius = 50
     segments = 360*10
-    color = (1, 0, 0)
+    fill_color = (1, 0, 0)
+    line_color = tuple([value - 0.5 for value in fill_color])
 
-    draw_circle(color, radius, position, segments)
+    draw_circle(fill_color, radius, position, segments)
+    draw_circle(line_color, radius, position, segments, 4, False)
 
     # restore opengl defaults
     bgl.glLineWidth(1)
@@ -44,7 +46,7 @@ def draw(position):
     bgl.glColor4f(0.0, 0.0, 0.0, 1.0)
 
 
-def draw_circle(color, radius, position, segments, fill=True):
+def draw_circle(color, radius, position, segments, line=4, fill=True):
 
     theta = 2 * math.pi / segments
     tangential_factor = math.tan(theta)
@@ -55,6 +57,7 @@ def draw_circle(color, radius, position, segments, fill=True):
     y = 0
 
     bgl.glColor3f(*color)
+    bgl.glLineWidth(line)
     bgl.glBegin(gl_type)
 
     for i in range(segments):
